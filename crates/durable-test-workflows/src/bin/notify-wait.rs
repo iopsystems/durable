aea1b7f1ec84de49616b8717faecd9f48a1453a2
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2024-09-06 14:34:46 -0700

    formatting

--- crates/durable-test-workflows/src/bin/notify-wait.rs
+++ crates/durable-test-workflows/src/bin/notify-wait.rs
@@ -3,9 +3,5 @@ use durable::notify;
 fn main() {
     let notif = notify::wait();
 
-    print!(
-        "event: {}\ndata:  {}\n",
-        notif.event,
-        notif.data.get(),
-    );
+    print!("event: {}\ndata:  {}\n", notif.event, notif.data.get(),);
 }
