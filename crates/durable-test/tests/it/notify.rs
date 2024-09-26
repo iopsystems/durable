338cf03a3c56fcc0f05d3fda59d44070981f378b
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2024-09-26 11:50:27 -0700

    Add test case

--- crates/durable-test/tests/it/notify.rs
+++ crates/durable-test/tests/it/notify.rs
@@ -104,3 +104,67 @@ async fn notify_after_suspend(pool: sqlx::PgPool) -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[sqlx::test]
+async fn notify_multiple_workers(pool: sqlx::PgPool) -> anyhow::Result<()> {
+    let client = DurableClient::new(pool.clone())?;
+    let program = crate::load_binary(&client, "notify-wait.wasm").await?;
+    let task = client
+        .launch("notify self test", &program, &serde_json::json!(null))
+        .await?;
+
+    let mut listener = PgListener::connect_with(&pool).await?;
+    listener.listen("durable:task-suspend").await?;
+
+    let _guard1 = durable_test::spawn_worker_with(
+        pool.clone(),
+        Config::new()
+            .suspend_margin(Duration::ZERO)
+            .suspend_timeout(Duration::ZERO),
+    )
+    .await?;
+
+    let _guard2 = durable_test::spawn_worker_with(
+        pool.clone(),
+        Config::new()
+            .suspend_margin(Duration::ZERO)
+            .suspend_timeout(Duration::ZERO),
+    )
+    .await?;
+
+    let future = async {
+        loop {
+            let _ = listener.try_recv().await?;
+
+            let suspended = sqlx::query_scalar!(
+                r#"
+                SELECT state = 'suspended' as "state!"
+                FROM durable.task
+                WHERE id = $1
+                "#,
+                task.id()
+            )
+            .fetch_one(&pool)
+            .await?;
+
+            if suspended {
+                break;
+            }
+        }
+
+        anyhow::Ok(())
+    };
+
+    tokio::time::timeout(Duration::from_secs(30), future)
+        .await
+        .context("task failed to suspend itself within 30s")??;
+
+    task.notify("notification", &(), &client).await?;
+
+    let status = tokio::time::timeout(Duration::from_secs(30), task.wait(&client))
+        .await
+        .context("task failed to complete in under 30s")??;
+    assert!(status.success());
+
+    Ok(())
+}
