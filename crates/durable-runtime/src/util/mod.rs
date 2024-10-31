18c6c1a2d41e6fd3ed81a450d350fbd9f056c233
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2024-10-31 13:50:38 -0700

    Add some basic metrics to the runtime
    It is useful to export some metrics when monitoring things. This change
    adds a few metrics, and paves the way for more in the future.

--- crates/durable-runtime/src/util/mod.rs
+++ crates/durable-runtime/src/util/mod.rs
@@ -1,9 +1,11 @@
 mod asyncfn;
 mod interval;
 mod mailbox;
+mod metrics;
 mod serde;
 
 pub use self::asyncfn::AsyncFnOnce;
 pub(crate) use self::interval::IntoPgInterval;
 pub(crate) use self::mailbox::Mailbox;
+pub(crate) use self::metrics::MetricSpan;
 pub(crate) use self::serde::EmptyMapDeserializer;
